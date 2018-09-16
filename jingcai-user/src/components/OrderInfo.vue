93fbf38dca8d08b198472ce182cb661459d7f639
Fred Goo <fredera92@gmail.com>
2018-09-16 15:34:16 +0800

    Fix: scale

--- jingcai-user/src/components/OrderInfo.vue
+++ jingcai-user/src/components/OrderInfo.vue
@@ -151,6 +151,20 @@
     methods: {
       // 初始化函数
       init() {
+        document.addEventListener('touchstart', function (event) {
+          if (event.touches.length > 1) {
+            event.preventDefault()
+          }
+        })
+        let lastTouchEnd = 0;
+        document.addEventListener('touchend', function (event) {
+          let now = (new Date()).getTime()
+          if (now - lastTouchEnd <= 300) {
+            event.preventDefault()
+          }
+          lastTouchEnd = now;
+        }, false)
+
         this.$axios
           .get('/config/getConfigByCategories', {
             params: {
